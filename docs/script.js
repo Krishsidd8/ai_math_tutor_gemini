caeaf8a36fb395875da6a67cc0b1dde4b542ede9
Krishsidd8 <krishsidd8@gmail.com>
2025-09-14 12:24:16 -0700

    fix

--- docs/script.js
+++ docs/script.js
@@ -103,7 +103,11 @@ imageInput.addEventListener('change', async (event) => {
   };
   reader.readAsDataURL(file);
 
-  spinnerMsg = addBotMessage(`<div class="loader-container"><div class="loader"></div></div>`);
+  spinnerMsg = addBotMessage(`
+  <div class="loader-container">
+    <div class="loader"></div>
+  </div>
+`);
 
   const formData = new FormData();
   formData.append("file", file);
